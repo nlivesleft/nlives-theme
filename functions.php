c4b66f1b4555612e75608e5d5c5d925b5bb7a1b7
saneeich <nlivesleft@gmail.com>
2017-07-15 16:06:07 +0530

    going back to existing footer

--- functions.php
+++ functions.php
@@ -10,23 +10,12 @@ require_once( get_template_directory() . '/lib/init.php' );
 add_action( 'beans_uikit_enqueue_scripts', 'beans_child_enqueue_uikit_assets' );
 
 function beans_child_enqueue_uikit_assets() {
-
-	beans_compiler_add_fragment( 'uikit', get_stylesheet_directory_uri() . '/style.less', 'less' );
-
+    beans_compiler_add_fragment( 'uikit', get_stylesheet_directory_uri() . '/style.less', 'less' );
 }
 
 // Remove this action and callback function if you are not adding CSS in the style.css file.
 add_action( 'wp_enqueue_scripts', 'beans_child_enqueue_assets' );
 
 function beans_child_enqueue_assets() {
-
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
-
-}
-
-beans_modify_action_callback( 'beans_footer_partial_template', 'custom_footer' );
-function custom_footer() {
-  ?>
-  <div class="test">test footer</div>
-  <?php
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
 }
